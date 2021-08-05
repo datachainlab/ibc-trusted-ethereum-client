4bb7ac7abedb96fafe3f5da42762f517a67b5b10
Toshihiko Okubo <toshihiko.okubo@datachain.jp>
2021-08-05 10:33:14 +0900

    add github workflow

--- modules/light-clients/xx-ethereum/module.go
+++ modules/light-clients/xx-ethereum/module.go
@@ -1,10 +1,6 @@
 package ethereum
 
-import (
-	"github.com/datachainlab/ibc-trusted-ethereum-client/modules/light-clients/xx-ethereum/types"
-)
-
 // Name returns the IBC client name
 func Name() string {
-	return types.SubModuleName
+	return "" // FIXME
 }
